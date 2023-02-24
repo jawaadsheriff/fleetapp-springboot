8371c0600db0a8f95ab20bf721ec5c92c90cd854
jawaadsheriff <jawaad.engineer@gmail.com>
2023-02-24 19:23:35 +0530

    EncryptedPwd

--- fleetapp/src/main/java/com/sheriff/fleetapp/ApplicationSecurityConfig.java
+++ fleetapp/src/main/java/com/sheriff/fleetapp/ApplicationSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -43,6 +44,11 @@ public class ApplicationSecurityConfig{
 	public PasswordEncoder passwordEncoder() {
 		return NoOpPasswordEncoder.getInstance();
 	}
+	
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	@Autowired UserDetailsService userDetailsService;
 
@@ -50,7 +56,7 @@ public class ApplicationSecurityConfig{
 	public AuthenticationProvider authenticationProvider() {
 		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
 		provider.setUserDetailsService(userDetailsService);
-		provider.setPasswordEncoder(passwordEncoder());
+		provider.setPasswordEncoder(bCryptPasswordEncoder());
 		return provider;
 	}
 
--- fleetapp/src/main/java/com/sheriff/fleetapp/services/UserService.java
+++ fleetapp/src/main/java/com/sheriff/fleetapp/services/UserService.java
@@ -1,6 +1,7 @@
 package com.sheriff.fleetapp.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.sheriff.fleetapp.models.User;
@@ -12,7 +13,11 @@ public class UserService {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
 	public void saveUser(User user) {
+		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
 		userRepository.save(user);
 	}
 }
